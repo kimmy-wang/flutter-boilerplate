4a80d50d3335add52dcb507d33e4f1423da18fd5
see <upcwangying@126.com>
2022-09-07 09:02:00 +0800

    refactor: trending page

--- lib/modules/trending/views/trending_page.dart
+++ lib/modules/trending/views/trending_page.dart
@@ -46,41 +46,45 @@ class _TrendingViewState extends State<TrendingView> {
   @override
   Widget build(BuildContext context) {
     return Platform.isMacOS
-        ? MacosScaffold(
-            toolBar: ToolBar(
-              title: const Text('Trending'),
-              titleWidth: 150.0,
-              leading: MacosTooltip(
-                message: 'Toggle Sidebar',
-                useMousePosition: false,
-                child: MacosIconButton(
-                  icon: MacosIcon(
-                    CupertinoIcons.sidebar_left,
-                    color: MacosTheme.brightnessOf(context).resolve(
-                      const Color.fromRGBO(0, 0, 0, 0.5),
-                      const Color.fromRGBO(255, 255, 255, 0.5),
-                    ),
-                    size: 20.0,
-                  ),
-                  boxConstraints: const BoxConstraints(
-                    minHeight: 20,
-                    minWidth: 20,
-                    maxWidth: 48,
-                    maxHeight: 38,
-                  ),
-                  onPressed: () => MacosWindowScope.of(context).toggleSidebar(),
-                ),
-              ),
-            ),
-            children: [
-              ContentArea(builder: (context, scrollController) {
-                return _body;
-              })
-            ],
-          )
+        ? _macosScaffold
         : Scaffold(
             body: _body,
           );
+  }
+
+  Widget get _macosScaffold {
+    return MacosScaffold(
+      toolBar: ToolBar(
+        title: const Text('Trending'),
+        titleWidth: 150.0,
+        leading: MacosTooltip(
+          message: 'Toggle Sidebar',
+          useMousePosition: false,
+          child: MacosIconButton(
+            icon: MacosIcon(
+              CupertinoIcons.sidebar_left,
+              color: MacosTheme.brightnessOf(context).resolve(
+                const Color.fromRGBO(0, 0, 0, 0.5),
+                const Color.fromRGBO(255, 255, 255, 0.5),
+              ),
+              size: 20.0,
+            ),
+            boxConstraints: const BoxConstraints(
+              minHeight: 20,
+              minWidth: 20,
+              maxWidth: 48,
+              maxHeight: 38,
+            ),
+            onPressed: () => MacosWindowScope.of(context).toggleSidebar(),
+          ),
+        ),
+      ),
+      children: [
+        ContentArea(builder: (context, scrollController) {
+          return _body;
+        })
+      ],
+    );
   }
 
   Widget get _body {
