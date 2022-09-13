a3beb61ede18603a92fa308d4f5032b1c4203431
kimmy214 <upcwangying@126.com>
2022-09-13 15:55:51 +0800

    fix: TrendingHttpBaseClient

--- lib/common/http/clients.dart
+++ lib/common/http/clients.dart
@@ -2,13 +2,19 @@ import 'package:flutter_boilerplate/common/constants/constants.dart';
 import 'package:http_trending_api/http_trending_api.dart';
 
 class TrendingHttpBaseClient extends AbstractBaseClient {
-  TrendingHttpBaseClient(String baseUrl, {Client? inner})
-      : _inner = inner ?? Client(), super(baseUrl);
+  TrendingHttpBaseClient(super.baseUrl, {Client? inner})
+      : _inner = inner ?? Client();
 
   final Client _inner;
 
   Future<StreamedResponse> send(BaseRequest request) {
     request.headers.addAll(Constants.headers);
     return _inner.send(request);
+  }
+
+  @override
+  void close() {
+    _inner.close();
+    super.close();
   }
 }
